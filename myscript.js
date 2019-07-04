0819938994b5e54e389569c67e895d12568b575f
enrico76 <enricodip@gmail.com>
2019-07-04 09:39:25 +0200

    aggiunta descrizione esercizio

--- myscript.js
+++ myscript.js
@@ -1,3 +1,7 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+// E scrivi sulla pagina nomecognomecolorepreferito19
+
+
 var nome = prompt("qual è il tuo nome?");
 console.log(nome);
 var cognome = prompt("qual è il tuo cognome?");
