dc1438962e03cc6c6da709b7c9c72341d42f0833
enrico76 <enricodip@gmail.com>
2019-07-04 09:31:07 +0200

    completato

---
+++ myscript.js
@@ -0,0 +1,7 @@
+var nome = prompt("qual è il tuo nome?");
+console.log(nome);
+var cognome = prompt("qual è il tuo cognome?");
+console.log(cognome);
+var colore = prompt("il tuo colore preferito?");
+console.log(colore);
+document.getElementById('pw').innerHTML = nome + cognome + colore + 19;
